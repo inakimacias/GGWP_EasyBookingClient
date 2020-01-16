eb1fc6ace3059f29613597d04837c0a6cdfd8986
Daniel Diez-Andino Lopez <diezandino.daniel@opendeusto.es>
2020-01-16 09:56:36 +0100

    RMI

--- src/remote/ServiceLocator.java
+++ src/remote/ServiceLocator.java
@@ -4,23 +4,23 @@ import java.rmi.Naming;
 
 public class ServiceLocator {
 	
-//	private IEasyBookingManager service;
-//	
-//	public void setService(String ip, String port, String serverName) {
-//		if (System.getSecurityManager() == null) {
-//			System.setSecurityManager(new SecurityManager());
-//		}
-//		
-//		try {		
-//			String URL = "//" + ip + ":" + port + "/" + serverName;
-//			this.service = (IEasyBookingManager) Naming.lookup(URL);
-//		} catch (Exception ex) {
-//			System.err.println("# Error locating remote fa�ade: " + ex);
-//		}		
-//	}
-//
-//	public IEasyBookingManager getService() {
-//		return this.service;
-//	}
+	private IEasyBookingManager service;
+	
+	public void setService(String ip, String port, String serverName) {
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new SecurityManager());
+		}
+		
+		try {		
+			String URL = "//" + ip + ":" + port + "/" + serverName;
+			this.service = (IEasyBookingManager) Naming.lookup(URL);
+		} catch (Exception ex) {
+			System.err.println("# Error locating remote fa�ade: " + ex);
+		}		
+	}
+
+	public IEasyBookingManager getService() {
+		return this.service;
+	}
 
 }
