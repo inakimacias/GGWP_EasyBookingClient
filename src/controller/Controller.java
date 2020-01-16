2829747565f5b8756d8a8364d8f939fc4fb81e5f
Daniel Diez-Andino Lopez <diezandino.daniel@opendeusto.es>
2020-01-16 10:07:20 +0100

    RMI

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -6,19 +6,19 @@ import java.util.List;
 import remote.ServiceLocator;
 
 public class Controller {
-//	public ServiceLocator serviceLocator;
-//	
-//	public Controller(ServiceLocator serviceLocator) {
-//		this.serviceLocator = serviceLocator; 
-//	}
-//	
-//	public boolean login(String email, String password) {
-//		try {
-//			return this.serviceLocator.getService().login(email, password);
-//		} catch (RemoteException e) {
-//			System.err.println("# Error during login: " + e);
-//			return false;
-//		}
-//	}
+	public ServiceLocator serviceLocator;
+	
+	public Controller(ServiceLocator serviceLocator) {
+		this.serviceLocator = serviceLocator; 
+	}
+	
+	public boolean login(String email, String password) {
+		try {
+			return this.serviceLocator.getService().login(email, password);
+		} catch (RemoteException e) {
+			System.err.println("# Error during login: " + e);
+			return false;
+		}
+	}
 	
 }
